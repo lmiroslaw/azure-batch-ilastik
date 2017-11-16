0cce49d408d028a1d0691c8fa5525660da13cce7
Lukasz Miroslaw <lmiroslaw@github.com>
2017-11-16 14:03:25 +0100

    Cleaned up the 00 file

--- 00.Upload.sh
+++ 00.Upload.sh
@@ -4,7 +4,7 @@ echo $workspace
 cd $workspace
 tar -cf runme.tar pixelClassification.ilp run_task.sh
 echo "Uploading"
-az storage blob upload -f runme.tar --account-name shipyarddata --account-key O3SgBchy4x4Ms1UDDpSpeHVy+G9Ah2IWanRHppyJaQoUG3aPOTP9q+AcW0YfyZA/KEtyYnsLjQEG5yk9PYZilQ== -c drosophila --name runme.tar
+az storage blob upload -f runme.tar --account-name shipyarddata --account-key longkey== -c drosophila --name runme.tar
 cd ..
 echo "Uploading"
-az storage blob upload -f deploy_script.sh --account-name shipyarddata --account-key O3SgBchy4x4Ms1UDDpSpeHVy+G9Ah2IWanRHppyJaQoUG3aPOTP9q+AcW0YfyZA/KEtyYnsLjQEG5yk9PYZilQ== -c drosophila --name deploy_script.sh
+az storage blob upload -f deploy_script.sh --account-name shipyarddata --account-key longkey== -c drosophila --name deploy_script.sh
