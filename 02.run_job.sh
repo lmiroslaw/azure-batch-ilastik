9a0f9b429b2c0e42a62ebfb1f8be5283fd60bca5
Lukasz Miroslaw <lumirosl@microsoft.com>
2018-02-08 10:08:02 +0100

    Modified after azure batch login

--- 02.run_job.sh
+++ 02.run_job.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 JOBID=`date +%Y-%m-%d_%H_%M`
-#echo 'creating job $JOBID...'
-az batch job create --id $JOBID --pool-id ilastik --account-endpoint https://matlabb.westeurope.batch.azure.com --account-name matlabb 
+export GROUPID=demorg
+export BATCHID=matlabb
+az batch account login -g $GROUPID -n $BATCHID
+
+echo 'creating job $JOBID...'
+az batch job create --id $JOBID --pool-id ilastik  
 for k in {1..2} 
   do 
     echo "starting task_$k ..."
-    az batch task create --job-id $JOBID --task-id "task_$k" --command-line "/mnt/batch/tasks/shared/run_task.sh $k > out.log" --account-endpoint https://matlabb.westeurope.batch.azure.com --account-name matlabb
+    az batch task create --job-id $JOBID --task-id "task_$k" --command-line "/mnt/batch/tasks/shared/run_task.sh $k > out.log" 
   done
 echo "DONE. JOBID=${JOBID} executed."
--- 03.download_output.sh
+++ 03.download_output.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+export GROUPID=demorg
+export BATCHID=matlabb
+az batch account login -g $GROUPID -n $BATCHID
 
-JOBID=$1
+JOBI=D$1
 filename=drosophila_00-49
 
 for k in {1..2}; do
 	for j in {0..9}; do
 	  echo "Downloading ${filename}0${j}.tiff from task_${k} ..."
-	  az batch task file download --job-id $JOBID --task-id task_${k} --file-path wd/${filename}0${j}.tiff --destination ~/ilastikresults/${k}/${filename}0${j}.tiff --account-endpoint https://matlabb.westeurope.batch.azure.com --account-name matlabb
+	  az batch task file download --job-id $JOBID --task-id task_${k} --file-path wd/${filename}0${j}.tiff --destination ~/ilastikresults/${k}/${filename}0${j}.tiff 
 	 done
 	for j in {10..49}; do
 	  echo "Downloading ${filename}${j}.tiff from task_${k} ..."
-	  az batch task file download --job-id $JOBID --task-id task_${k} --file-path wd/${filename}${j}.tiff --destination ~/ilastikresults/${k}/${filename}${j}.tiff --account-endpoint https://matlabb.westeurope.batch.azure.com --account-name matlabb
+	  az batch task file download --job-id $JOBID --task-id task_${k} --file-path wd/${filename}${j}.tiff --destination ~/ilastikresults/${k}/${filename}${j}.tiff 
 	 done
 done	 
 echo "done."
