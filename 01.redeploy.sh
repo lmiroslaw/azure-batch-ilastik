3be12650542149f2438b305d9d030f1481c78911
Lukasz Miroslaw <lmiroslaw@github.com>
2018-03-05 13:12:08 +0100

    updated 01.redeploy.sh

--- 01.redeploy.sh
+++ 01.redeploy.sh
@@ -2,15 +2,19 @@
 # Deploy a new Batch cluster with Ubuntu 
 
 poolid=$1
+rgid=
+batchid=
+batchep=
 
-az batch pool create --account-name matlabb --account-endpoint https://matlabb.westeurope.batch.azure.com --id ${poolid} --image "Canonical:UbuntuServer:16.04.0-LTS" --node-agent-sku-id "batch.node.ubuntu 16.04"  --vm-size Standard_D11 --verbose
+az batch account login -g $rgid -n $batchid --verbose
+az batch pool create --id ${poolid} --image "Canonical:UbuntuServer:16.04.0-LTS" --node-agent-sku-id "batch.node.ubuntu 16.04"  --vm-size Standard_D11 --verbose
 
 # Assign a json to a pool
-az batch pool set --pool-id ${poolid} --json-file pool-shipyard.json --account-endpoint https://matlabb.westeurope.batch.azure.com --account-name matlabb
+az batch pool set --pool-id ${poolid} --json-file pool-shipyard.json --verbose
 
 # Resize a pool
-az batch pool resize --pool-id ${poolid} --target-dedicated 2 --account-endpoint https://matlabb.westeurope.batch.azure.com --account-name matlabb
+az batch pool resize --pool-id ${poolid} --target-dedicated 2 --verbose 
 
 # Remove the old pool if necessary
-# az batch pool delete --pool-id ${poolid}  --account-endpoint https://matlabb.westeurope.batch.azure.com --account-name matlabb
+# az batch pool delete --pool-id ${poolid} 
 
